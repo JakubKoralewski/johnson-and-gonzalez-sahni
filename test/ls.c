c9b55d9e7602a612fd1b07183d1024ff7c5c6715
Jakub Koralewski <jakkor8@st.amu.edu.pl>
2021-11-30 19:36:12 +0100

    fix ls tests

--- test/ls.c
+++ test/ls.c
@@ -6,39 +6,41 @@ int main() {
 	Input input;
 	Job jobs[3] = {
 		{
-			.j = 2,
-			.prdw = {2, 0, 0, 0}
-		},
-		{
 			.j = 1,
 			.prdw = {1, 0, 0, 0},
+		},
+		{
+			.j = 2,
+			.prdw = {2, 0, 0, 0}
 		},
 		{
 			.j = 3,
 			.prdw = {1, 0, 0, 0}
 		}
 	};
-	INDEX machine_assignments[] = {2,2,1};
+	INDEX machine_assignments[] = {1,2,1};
 	input.buffer = jobs;
 	int len = sizeof(jobs) / sizeof(Job);
 	input.length = len;
 	input.which_set = 1 << P;
+	MACHINE num_machines = 2;
 
 	Schedule schedule;
 
-	if ((rv = imp2(&schedule, &input, "erd"))) {
-		assert(rv == 0, "%s", "imp1() call should succeed");
+	if ((rv = imp2(num_machines, &schedule, &input, "ls"))) {
+		assert(rv == 0, "%s", "imp2() call should succeed");
 	}
 	for (int i = 0; i < len; i++) {
-		assert(schedule.input->buffer[i].j == i + 1, "%s (len %d) was %zu %zu %zu", "1,2,3 order", len,
-		       input.buffer[0].j, input.buffer[1].j, input.buffer[2].j);
+		INDEX index_of_job = schedule.input->buffer[i].j;
+		MACHINE m_id = machine_assignments[index_of_job-1];
+		assert_eq(schedule.input->buffer[i].m_id, m_id, "%u", "%u", "i: %d", i);
 	}
-	assert(schedule.schedule[0].start == 2, "%s", "ready time of first job");
-	assert(schedule.schedule[0].end == 3, "%s", "processing time of 1st job");
+	assert(schedule.schedule[0].start == 0, "%s", "ready time of first job");
+	assert(schedule.schedule[0].end == 1, "%s", "processing time of 1st job");
 
-	assert(schedule.schedule[1].start == 4, "%s", "ready time of 2nd job");
-	assert(schedule.schedule[1].end == 6, "%s", "p of 2nd job");
+	assert(schedule.schedule[1].start == 0, "%s", "ready time of 2nd job");
+	assert(schedule.schedule[1].end == 2, "%s", "p of 2nd job");
 
-	assert(schedule.schedule[2].start == 6, "%s", "end of 2nd job");
-	assert(schedule.schedule[2].end == 7, "%s", "p t of 3rd job");
+	assert(schedule.schedule[2].start == 1, "%s", "end of 2nd job");
+	assert(schedule.schedule[2].end == 2, "%s", "p t of 3rd job");
 }
